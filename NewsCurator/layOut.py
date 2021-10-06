3a6e695efb025df428957f30cce7c58c395e69bf
Rolling Stone <aRollingStone27@gmail.com>
2021-10-07 01:11:16 +0900

    파일분할리팩토링시작

--- NewsCurator/layOut.py
+++ NewsCurator/layOut.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkcalendar import Calendar
+from functionTest import *
 
 
 # 메인
@@ -70,42 +71,7 @@ def searchFrameMake():
     searchButton = Button(searchFrame, text="검색", width=20, bg="green", fg="white", command=searchNews)
     searchButton.pack(side="left", padx=20)        
 
-startCount = 0
-endCount = 0
 
-def startCal():    
-    global startCal
-    global startCount    
-    if startCount == 1:
-        startCount = 0
-        startCal.place_forget()
-    else:       
-        startCal = Calendar(root)
-        startCount = startCount + 1
-        startCal.pack(pady=10)
-        startCal.place(x=5, y=240)
-
-def endCal():    
-    global endCal
-    global endCount
-    if endCount == 1:
-        endCount = 0
-        endCal.place_forget()
-    else:
-        endCal = Calendar(root)
-        endCount = endCount + 1    
-        endCal.pack(pady=10)
-        endCal.place(x=230, y=240)
-
-def removeCal():
-    global startCount
-    global endCount
-    if startCount == 1:
-        startCount = 0
-        startCal.place_forget()
-    if endCount == 1:
-        endCount = 0
-        endCal.place_forget()
 
 def searchNews():
     global startCount    
