eee1ee5182d0634d1fe223c3504d67f2c4c9cb46
Rolling Stone <aRollingStone27@gmail.com>
2021-10-11 20:49:27 +0900

    달력 날짜 예외처리, 모달, 디자인 반영x

--- NewsCurator2/layOut.py
+++ NewsCurator2/layOut.py
@@ -1,21 +1,25 @@
 from tkinter import *
 from tkcalendar import Calendar
 import pyautogui
+from datetime import *
 
 # 메인
 def newsCuratorRun():
     global root
     global headerFrame
+    global searchFrame    
+
     root = Tk()
     root.title("News Curator")
     root.geometry("1100x800")
     root.resizable(False, False)
-    root.configure(bg="green")
+    root.configure(bg="#284922")
+    
     headerFrame = HeaderFrame(root)
     NewsFrame(root)                     
     SectionFrame(root)
     AdFrame(root)
-    SearchFrame(root)
+    searchFrame = SearchFrame(root)
     ListFrame(root)
     FooterFrame(root)    
     root.mainloop()
@@ -24,20 +28,20 @@ def newsCuratorRun():
 class FooterFrame():
 
     def __init__(self, master):        
-        footerFrame = Frame(master, bg="green")
+        footerFrame = Frame(master, bg="#284922")
         footerFrame.pack(fill="x", padx=5, pady=5)
         footerFrame.place(x=10, y=750)
         
-        createLabel = Label(footerFrame, text="POWERD by Holistic Python", width=30, fg="white", bg="green", anchor=W, font="Helvetica 15 bold")
+        createLabel = Label(footerFrame, text="POWERD by Holistic Python", width=30, fg="white", bg="#284922", anchor=W, font="Helvetica 15 bold")
         createLabel.pack(side="left")
         
-        sortButton = Button(footerFrame, text="  ↘  정렬  ↗  ", width=50, bg="green", fg="white")
+        sortButton = Button(footerFrame, text="  ↘  정렬  ↗  ", width=50, bg="#284922", fg="white")
         sortButton.pack(side="left", padx=20)
         
-        emailButton = Button(footerFrame, text="이메일발송", width=18, bg="green", fg="white")
+        emailButton = Button(footerFrame, text="이메일발송", width=18, bg="#284922", fg="white")
         emailButton.pack(side="left", padx=6)
         
-        saveButton = Button(footerFrame, text="csv저장", width=18, bg="green", fg="white")
+        saveButton = Button(footerFrame, text="csv저장", width=18, bg="#284922", fg="white")
         saveButton.pack(side="left", padx=6)
 
 # 리스트 프레임
@@ -45,13 +49,13 @@ class ListFrame():
 
     def __init__(self, master):        
 
-        listFrame = Frame(master, bg="green")
+        listFrame = Frame(master, bg="#284922")
         listFrame.pack(fill="both", padx=5, pady=5)
         listFrame.place(x=10, y=250)
         scrollbar = Scrollbar(listFrame)
         scrollbar.pack(side="right", fill="y")
 
-        newsList = Listbox(listFrame, selectmode="extended", width=150, height=30, yscrollcommand=scrollbar.set, bg="green")
+        newsList = Listbox(listFrame, selectmode="extended", width=150, height=30, yscrollcommand=scrollbar.set, bg="#284922")
         newsList.pack(fill="both", expand=True)
         scrollbar.config(command=newsList.yview)
 
@@ -59,7 +63,7 @@ class ListFrame():
 class CalMake():    
 
     def __init__(self, master):
-        self.newsCal = Calendar(master)
+        self.newsCal = Calendar(master, date_pattern = "ymmdd")
         self.newsCal.pack(pady=10)    
         
 # 검색 프레임
@@ -70,38 +74,69 @@ class SearchFrame():
     endCount = 0        
 
     def __init__(self, master):
-        searchFrame = Frame(master, bg="green")
-        searchFrame.pack(pady=5, fill="x")
-        searchFrame.place(x=10, y=210)        
+        self.searchFrame = Frame(master, bg="#284922")
+        self.searchFrame.pack(pady=5, fill="x")
+        self.searchFrame.place(x=10, y=210)        
         
-        startDateButton = Button(searchFrame, text="시작날짜", width=20, bg="green", fg="white", command=self.startCalMake)
+        startDateButton = Button(self.searchFrame, text="시작날짜", width=20, bg="#284922", fg="white", command=self.startCalMake)
         startDateButton.pack(side="left", padx=20)
         
-        endDateButton = Button(searchFrame, text="종료날짜", width=20, bg="green", fg="white", command=self.endCalMake)
+        endDateButton = Button(self.searchFrame, text="종료날짜", width=20, bg="#284922", fg="white", command=self.endCalMake)
         endDateButton.pack(side="left", padx=20)
 
-        searchEntry = Entry(searchFrame, width=30)
+        searchEntry = Entry(self.searchFrame, width=30)
         searchEntry.pack(side="left", padx=20)
 
-        searchButton = Button(searchFrame, text="검색", width=20, bg="green", fg="white", command=self.removeCal)
+        searchButton = Button(self.searchFrame, text="검색", width=20, bg="#284922", fg="white", command=self.search)
         searchButton.pack(side="left", padx=20)
 
+    # 검색날짜 세팅
+    def dateSet(self):
+
+        now = datetime.now().strftime("%Y%m%d")
+
+        # 시작날짜(없으면 오늘)
+        if self.startCount != 0:
+           startDate = self.startCal.newsCal.get_date()       
+        else:
+            startDate = now
+
+        # 종료날짜(없으면 오늘)
+        if self.endCount != 0:
+            endDate = self.endCal.newsCal.get_date()
+        else:
+            endDate = now
+        
+        if int(endDate) > int(now):
+            endDate = now       
+        
+        if int(startDate) > int(endDate):
+            root.wm_attributes("-disabled", True)
+            CalError(root)  
+
+    # 검색    
+    def search(self):             
+        self.dateSet()
+        self.removeCal()
+
+    # 시작달력
     def startCalMake(self):        
            
         if self.startCount == 0:
             self.startCal = CalMake(root)
             self.startCount = 1
-            self.startCal.newsCal.place(x=5, y=240)
+            self.startCal.newsCal.place(x=5, y=240)                     
         else:
             self.startCal.newsCal.destroy()
             self.startCount = 0
 
+    # 종료 달력
     def endCalMake(self):
 
         if self.endCount == 0:
             self.endCal = CalMake(root)
             self.endCount = 1
-            self.endCal.newsCal.place(x=230, y=240)
+            self.endCal.newsCal.place(x=230, y=240)            
         else:
             self.endCal.newsCal.destroy()
             self.endCount = 0
@@ -118,12 +153,30 @@ class SearchFrame():
             self.endCount = 0
             self.endCal.newsCal.destroy()            
 
+# 달력 에러 알림창 modal 적용
+class CalError():   
+
+    def __init__(self, master):
+        self.calModal = Toplevel(master)
+        self.x, self.y = pyautogui.position()
+        self.calModal.geometry(f"{300}x{90}+{self.x-450}+{self.y+50}")
+        self.calModal.transient(master)
+        self.errorLabel = Label(self.calModal, text="날짜 정보를 올바르게 입력해주세요.", fg="black", font='Helvetica 12', pady=10, padx=10)
+        self.errorLabel.pack()
+        self.errorLabel1 = Label(self.calModal, text="종료날짜가 시작날짜 이전입니다.", fg="black", font='Helvetica 12', pady=10, padx=10)
+        self.errorLabel1.pack()
+        self.calModal.protocol("WM_DELETE_WINDOW", self.close)         
+
+    def close(self):
+        root.wm_attributes("-disabled", False)
+        self.calModal.destroy()     
+    
 # 광고 프레임
 class AdFrame():
 
     def __init__(self, master):
-        adFrame = Frame(master, bg="green", highlightthickness=2, highlightcolor="white")
-        adLabel=Label(adFrame, width=7, text="광고삽입", font='Helvetica 35 bold', fg="white", bg="green", anchor=CENTER, pady=50, padx=5)
+        adFrame = Frame(master, bg="#284922", highlightthickness=2, highlightcolor="white")
+        adLabel=Label(adFrame, width=7, text="광고삽입", font='Helvetica 35 bold', fg="white", bg="#284922", anchor=CENTER, pady=50, padx=5)
         adLabel.pack()
         adFrame.pack(pady=5)
         adFrame.place(x=860, y=70)            
@@ -135,15 +188,15 @@ class SectionFrame():
 
         self.sectionVar = StringVar()
 
-        sectionFrame = Frame(master, bg="green", highlightthickness=2, highlightcolor="white")    
+        sectionFrame = Frame(master, bg="#284922", highlightthickness=2, highlightcolor="white")    
         sectionFrame.pack(pady=5)
         sectionFrame.place(x=10, y=140)    
 
-        radioSociety = Radiobutton(sectionFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="사회", value="사회", bg="green", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 0)    
-        radioPolitics = Radiobutton(sectionFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="정치", value="정치", bg="green", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 1)
-        radioEconomy = Radiobutton(sectionFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="경제", value="경제", bg="green", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 2)
-        radioIT = Radiobutton(sectionFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="IT", value="IT", bg="green", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 3)
-        radioSports = Radiobutton(sectionFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="스포츠", value="스포츠", bg="green", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 4)   
+        radioSociety = Radiobutton(sectionFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="사회", value="사회", bg="#284922", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 0)    
+        radioPolitics = Radiobutton(sectionFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="정치", value="정치", bg="#284922", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 1)
+        radioEconomy = Radiobutton(sectionFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="경제", value="경제", bg="#284922", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 2)
+        radioIT = Radiobutton(sectionFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="IT", value="IT", bg="#284922", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 3)
+        radioSports = Radiobutton(sectionFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="스포츠", value="스포츠", bg="#284922", fg="white", variable=self.sectionVar, pady=15).grid(row=0, column = 4)   
 
 # 뉴스 프레임
 class NewsFrame():    
@@ -151,15 +204,15 @@ class NewsFrame():
     def __init__(self, master) -> None:
 
         self.newsVar = StringVar()
-        newsFrame = Frame(master, bg="green", highlightcolor="white", highlightthickness=2)
+        newsFrame = Frame(master, bg="#284922", highlightcolor="white", highlightthickness=2)
         newsFrame.pack(pady=5)
         newsFrame.place(x=10, y=70)        
 
-        radioChosun = Radiobutton(newsFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="조선일보", value="조선일보", bg="green", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=0)
-        radioJungang = Radiobutton(newsFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="중앙일보", value="중앙일보", bg="green", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=1)
-        radioDonga = Radiobutton(newsFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="동아일보", value="동아일보", bg="green", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=2)
-        radioHan = Radiobutton(newsFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="한겨레", value="한겨레", bg="green", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=3)
-        radioKyung = Radiobutton(newsFrame, selectcolor="green", activebackground = "green", width=15, font=15, text="경향", value="경향", bg="green", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=4) 
+        radioChosun = Radiobutton(newsFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="조선일보", value="조선일보", bg="#284922", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=0)
+        radioJungang = Radiobutton(newsFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="중앙일보", value="중앙일보", bg="#284922", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=1)
+        radioDonga = Radiobutton(newsFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="동아일보", value="동아일보", bg="#284922", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=2)
+        radioHan = Radiobutton(newsFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="한겨레", value="한겨레", bg="#284922", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=3)
+        radioKyung = Radiobutton(newsFrame, selectcolor="#284922", activebackground = "#284922", width=15, font=15, text="경향", value="경향", bg="#284922", fg="white", variable=self.newsVar, pady=15).grid(row=0, column=4) 
 
     def selectedNews(self):
         print(self.newsVar.get())
@@ -168,25 +221,26 @@ class NewsFrame():
 class LogInFrame():
 
     def __init__(self, master):        
-        self.logIn = Frame(master, bg="green")
+        self.logIn = Frame(master, bg="#284922")
         self.logIn.pack(fill="x")
 
-        idLabel = Label(self.logIn, text="아이디", width=8, fg="white", bg="green", font=10, padx=4)
+        idLabel = Label(self.logIn, text="아이디", width=8, fg="white", bg="#284922", font=10, padx=4)
         idLabel.pack(side="left")
         idEntry = Entry(self.logIn, width=12, font=10)
         idEntry.pack(side="left")
 
-        goButton = Button(self.logIn, text="Go", width=6, fg="white", bg="green", font=10)
+        goButton = Button(self.logIn, text="Go", width=6, fg="white", bg="#284922", font=10)
         goButton.pack(side="right", padx=15)
 
         pwEntry = Entry(self.logIn, width=14, font=10)
-        pwLabel = Label(self.logIn, width=8, text="비밀번호", fg="white", bg="green", font=10, padx=4)
+        pwLabel = Label(self.logIn, width=8, text="비밀번호", fg="white", bg="#284922", font=10, padx=4)
         pwEntry.pack(side="right")
         pwLabel.pack(side="right")
         self.logIn.place(x=600, y=12)
 
 # 회원 가입 창  
 def doJoin(x, y):
+    
     global joinWindow
     joinWindow = Tk()
     joinWindow.title("회원가입")
@@ -204,18 +258,18 @@ class HeaderFrame():
     joinCount = 0               
 
     def __init__(self, master):
-        self.header = Frame(master, bg="green")
+        self.header = Frame(master, bg="#284922")
         self.header.pack(fill="x", padx=5, pady=5, ipadx=10, ipady=10)
-        self.header.newsCuratorLabel = Label(self.header, text="뉴스 큐레이터", width=80, fg="white", bg="green", anchor=W, font='Helvetica 20 bold')
+        self.header.newsCuratorLabel = Label(self.header, text="뉴스 큐레이터", width=80, fg="white", bg="#284922", anchor=W, font='Helvetica 20 bold')
         self.header.newsCuratorLabel.pack(fill="x", side="left")
 
-        self.innerFrame = Frame(self.header, bg="green")
+        self.innerFrame = Frame(self.header, bg="#284922")
         self.innerFrame.place(x=800, y=12)
         
-        self.joinUs = Button(self.innerFrame, text="회원가입", width=10, fg="white", bg="green", padx=3, command=self.joinUs)        
-        self.joinUs.pack(side="right", padx=20)        
+        self.joinUsButton = Button(self.innerFrame, text="회원가입", width=10, fg="white", bg="#284922", padx=3, command=self.joinUs)        
+        self.joinUsButton.pack(side="right", padx=20)        
 
-        logInButton = Button(self.innerFrame, text="로그인", width=10, fg="white", bg="green", padx=3, command=self.logIn)
+        logInButton = Button(self.innerFrame, text="로그인", width=10, fg="white", bg="#284922", padx=3, command=self.logIn)
         logInButton.pack(side="right", padx=20)
 
     def logIn(self):
--- temp.py
+++ temp.py
@@ -1,26 +1,34 @@
 from tkinter import *
+from babel.dates import DateTimePattern
 from tkcalendar import Calendar
 import pyautogui
+from datetime import *
 #  객체생성
 root = Tk()
 # 제목설정
 root.title("News Curator")
 # 화면크기 설정
-root.geometry("1100x800")
+root.geometry("300x200")
 # 화면크기 변경 안됨
 root.resizable(False, False)
 # 배경화면 녹색
-# root.configure(bg="green")
+root.configure(bg="green")
 
-# 조선
-radioChosun = Radiobutton(width=15, font=15, text="조선일보", value="조선일보", fg="black", pady=15)
-radioChosun.pack()
-radioChosun1 = Radiobutton(width=15, font=15, text="조선일보", value="조선일보", fg="black", pady=15)
-radioChosun1.pack()
-testButton = Button(root, text="버튼", fg="black")
-testButton.pack()
-print(testButton.cget("text"))
+testCal = Calendar(root, date_pattern = "ymmdd")
+print(testCal.get_date())
+
+
+
+def modalTest():
+    root.wm_attributes("-disabled", True)    
+    modalWindow = Toplevel(root)
+    
+    modalWindow.geometry("300x200")
+    
+    modalWindow.transient(root)
+    modalLabel = Label(modalWindow, text="종료날짜가 시작날짜 이전입니다. 다시 선택해주세요")
+    modalLabel.pack(side="top")
+    modalButton = Button(root, text="모달테스트", fg="black", command=modalTest)
+    modalButton.pack()
+    
 root.mainloop()
-x, y = pyautogui.position()
-print(x)
-
